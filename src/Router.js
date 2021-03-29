847dd678f6e97648930fdc7a74f70693f165f7ea
bobbyg211 <robertdg96@gmail.com>
2021-03-29 19:31:52 -0400

    Redux added (not working)

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router";
 import cookie from "cookie";
-import Home from "./components/Home";
+import Home from "./containers/Home";
 import About from "./components/About";
 import Car from "./components/Car";
 import Login from "./components/Login";
